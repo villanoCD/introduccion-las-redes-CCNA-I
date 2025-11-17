63dc38c32e7363ae664c393136d4e26c324bf086
¨villanoCD¨ <villanocd@gmail.com>
2025-11-17 08:29:43 -0400

    primera parte

---
+++ ccna/pyhton/practica.py
@@ -0,0 +1,19 @@
+# aun no has nacido
+# bebe
+# niño/a
+# adolescente
+# joven
+# adulto/a joven
+# adulto/a mayor
+# adulto/a peliado
+# anciano/a
+# viviendo su mejor etapa
+# mentiroso/a
+
+# le damos la bienvenida al programa
+print('Bienvenido/a al programa de estados de un ser humano!')
+print('Desarrollado por: Vladimir')
+print('------------------------------------------')
+
+# pedimos la edad al usuario
+edad = input('Por favor, ingresa tu edad en años: ')
